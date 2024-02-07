750ef4ed1f879001f9d6fbafb3c5b1f5493b351c
kuna2512 <129643132+kuna2512@users.noreply.github.com>
2024-02-07 21:43:45 +0530

    ccc

---
+++ var.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=123;
+
+    cout<<a<<endl;
+
+    char b ='K';
+    cout<<b<<'\n';
+
+    bool bl = true;
+    cout<< bl << endl;
+}
